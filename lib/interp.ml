e56bef5b2d02553eafb5d94c0c728c484079967f
Wenjuan Zheng <zhengwenjuan1203@berkeley.edu>
2025-03-24 10:15:29 -0700

    1

--- lib/interp.ml
+++ lib/interp.ml
@@ -5,234 +5,166 @@ open Shared
 open Error
 
 (** A [value]  is the runtime value of an expression. *)
-type value
-  = Num of int
+type value =
+  | Num of int
   | Bool of bool
   | Pair of (value * value)
+  | Str of string
+  | InChannel of in_channel
+  | OutChannel of out_channel
 
-type environment =
-  value Symtab.symtab
+type environment = value Symtab.symtab
 
-let input_channel : in_channel ref =
-  ref stdin
-
-let output_channel : out_channel ref =
-  ref stdout
+let input_channel : in_channel ref = ref stdin
+let output_channel : out_channel ref = ref stdout
 
 let top_env : unit -> environment =
-  fun () ->
-    Symtab.empty
-      |> Symtab.add "true" (Bool true)
-      |> Symtab.add "false" (Bool false)
+ fun () ->
+  Symtab.empty
+  |> Symtab.add "true" (Bool true)
+  |> Symtab.add "false" (Bool false)
+  |> Symtab.add "stdin" (InChannel !input_channel) (* Added *)
+  |> Symtab.add "stdout" (OutChannel !output_channel)
+(* Added *)
 
 (** [display_value v] returns a string representation of the runtime value
     [v]. *)
 let rec display_value : value -> string =
-  fun v ->
-    begin match v with
-      | Num x ->
-          sprintf "%d" x
-
-      | Bool b ->
-          if b then "true" else "false"
-
-      | Pair (v1, v2) ->
-          sprintf "(pair %s %s)" (display_value v1) (display_value v2)
-    end
+ fun v ->
+  match v with
+  | Num x -> sprintf "%d" x
+  | Bool b -> if b then "true" else "false"
+  | Pair (v1, v2) ->
+      sprintf "(pair %s %s)" (display_value v1) (display_value v2)
+  | Str s -> sprintf "\"%s\"" (String.escaped s)
+  | InChannel _ -> "<in-channel>"
+  | OutChannel _ -> "<out-channel>"
 
 (** [interp_0ary_primitive prim] tries to evaluate the primitive operation
     named by [prim]. If [prim] does not refer to a valid primitive operation, it
     returns [None]. *)
 let interp_0ary_primitive : string -> value option =
-  fun prim ->
-    begin match prim with
-      | "read-num" ->
-          Some (Num (input_line !input_channel |> int_of_string))
-
-      | "newline" ->
-          output_string !output_channel "\n";
-          Some (Bool true)
-
-      | _ ->
-          None
-    end
+ fun prim ->
+  match prim with
+  | "read-num" -> Some (Num (input_line !input_channel |> int_of_string))
+  | "newline" ->
+      output_string !output_channel "\n";
+      Some (Bool true)
+  | _ -> None
 
 (** [interp_unary_primitive prim arg] tries to evaluate the primitive operation
     named by [prim] on the argument [arg]. If the operation is ill-typed, or if
     [prim] does not refer to a valid primitive operation, it returns [None]. *)
 let interp_unary_primitive : string -> value -> value option =
-  fun prim arg ->
-  begin match (prim, arg) with
-    | ("add1", Num x) ->
-        Some (Num (x + 1))
-
-    | ("sub1", Num x) ->
-        Some (Num (x - 1))
-
-    | ("zero?", Num 0) ->
-        Some (Bool true)
-
-    | ("zero?", _) ->
-        Some (Bool false)
-
-    | ("num?", Num _) ->
-        Some (Bool true)
-
-    | ("num?", _) ->
-        Some (Bool false)
-
-    | ("not", Bool false) ->
-        Some (Bool true)
-
-    | ("not", _) ->
-        Some (Bool false)
-
-    | ("pair?", Pair _) ->
-        Some (Bool true)
-
-    | ("pair?", _) ->
-        Some (Bool false)
-
-    | ("left", Pair (v, _)) ->
-        Some v
-
-    | ("right", Pair (_, v)) ->
-        Some v
-
-    | ("print", v) ->
-        v |> display_value |> output_string !output_channel;
-        Some (Bool true)
-
-    | _ ->
-        None
-  end
+ fun prim arg ->
+  match (prim, arg) with
+  | "add1", Num x -> Some (Num (x + 1))
+  | "sub1", Num x -> Some (Num (x - 1))
+  | "zero?", Num 0 -> Some (Bool true)
+  | "zero?", _ -> Some (Bool false)
+  | "num?", Num _ -> Some (Bool true)
+  | "num?", _ -> Some (Bool false)
+  | "not", Bool false -> Some (Bool true)
+  | "not", _ -> Some (Bool false)
+  | "pair?", Pair _ -> Some (Bool true)
+  | "pair?", _ -> Some (Bool false)
+  | "left", Pair (v, _) -> Some v
+  | "right", Pair (_, v) -> Some v
+  | "print", v ->
+      v |> display_value |> output_string !output_channel;
+      Some (Bool true)
+  | "open-in", Str filename -> Some (InChannel (open_in filename))
+  | "open-out", Str filename -> Some (OutChannel (open_out filename))
+  | "close-in", InChannel ch ->
+      close_in ch;
+      Some (Bool true)
+  | "close-out", OutChannel ch ->
+      close_out ch;
+      Some (Bool true)
+  | _ -> None
 
 (** [interp_binary_primitive prim arg1 arg2] tries to evaluate the primitive
     operation named by [prim] on the arguments [arg1] and [arg2]. If the
     operation is ill-typed, or if [prim] does not refer to a valid primitive
     operation, it returns [None]. *)
 let interp_binary_primitive : string -> value -> value -> value option =
-  fun prim arg1 arg2 ->
-    begin match (prim, arg1, arg2) with
-      | ("+", Num x1, Num x2) ->
-          Some (Num (x1 + x2))
-
-      | ("-", Num x1, Num x2) ->
-          Some (Num (x1 - x2))
-
-      | ("=", Num x1, Num x2) ->
-          Some (Bool (x1 = x2))
-
-      | ("<", Num x1, Num x2) ->
-          Some (Bool (x1 < x2))
-
-      | ("pair", v1, v2) ->
-          Some (Pair (v1, v2))
-
-      | _ ->
-          None
-    end
+ fun prim arg1 arg2 ->
+  match (prim, arg1, arg2) with
+  | "+", Num x1, Num x2 -> Some (Num (x1 + x2))
+  | "-", Num x1, Num x2 -> Some (Num (x1 - x2))
+  | "=", Num x1, Num x2 -> Some (Bool (x1 = x2))
+  | "<", Num x1, Num x2 -> Some (Bool (x1 < x2))
+  | "pair", v1, v2 -> Some (Pair (v1, v2))
+  | "input", InChannel ch, Num n when n >= 0 ->
+      let buf = Bytes.create n in
+      let bytes_read =
+        try
+          really_input ch buf 0 n;
+          n
+        with End_of_file -> 0
+      in
+      Some (Str (Bytes.sub_string buf 0 bytes_read))
+  | "output", OutChannel ch, Str s ->
+      output_string ch s;
+      flush ch;
+      Some (Bool true)
+  | _ -> None
 
 (** [interp_trinary_primitive prim arg1 arg2 arg3] tries to evaluate the
     primitive operation named by [prim] on the arguments [arg1], [arg2], and
     [arg3]. If the operation is ill-typed, or if [prim] does not refer to a
     valid primitive operation, it returns [None]. *)
-let interp_trinary_primitive :
- string -> value -> value -> value -> value option =
-  fun prim arg1 arg2 arg3 ->
-    begin match (prim, arg1, arg2, arg3) with
-      | _ ->
-          None
-    end
+let interp_trinary_primitive : string -> value -> value -> value -> value option
+    =
+ fun prim arg1 arg2 arg3 -> match (prim, arg1, arg2, arg3) with _ -> None
 
 (** [interp_expr e] tries to evaluate the s_expression [e], producing a
     value. If [e] isn't a valid expression, it raises an exception. *)
 let rec interp_expr : environment -> s_exp -> value =
-  fun env e ->
-    begin match e with
-      | Num x ->
-          Num x
-
-      | Sym var ->
-          begin match Symtab.find_opt var env with
-            | Some value ->
-                value
-
-            | None ->
-                raise (Stuck e)
-          end
-
-      | Lst [Sym "let"; Lst [Lst [Sym var; exp]]; body] ->
-          let env = env |> Symtab.add var (interp_expr env exp) in
-          interp_expr env body
-
-      | Lst [Sym "if"; test_exp; then_exp; else_exp] ->
-          if interp_expr env test_exp <> Bool false then
-            interp_expr env then_exp
-          else
-            interp_expr env else_exp
-
-      | Lst (Sym "do" :: exps) when List.length exps > 0 ->
-          exps
-            |> List.rev_map (interp_expr env)
-            |> List.hd
-
-      | Lst [Sym f]->
-          begin match interp_0ary_primitive f with
-            | Some v ->
-                v
-
-            | None ->
-                raise (Stuck e)
-          end
-
-      | Lst [Sym f; arg] ->
-          begin match interp_unary_primitive f (interp_expr env arg) with
-            | Some v ->
-                v
-
-            | None ->
-                raise (Stuck e)
-          end
-
-      | Lst [Sym f; arg1; arg2] ->
-        begin match
-          let v1 = interp_expr env arg1 in
-          let v2 = interp_expr env arg2 in
-          interp_binary_primitive f v1 v2
-        with
-          | Some v ->
-              v
-
-          | None ->
-              raise (Stuck e)
-        end
-
-      | Lst [Sym f; arg1; arg2; arg3] ->
-        begin match
-          let v1 = interp_expr env arg1 in
-          let v2 = interp_expr env arg2 in
-          let v3 = interp_expr env arg3 in
-          interp_trinary_primitive f v1 v2 v3
-        with
-          | Some v ->
-              v
-
-          | None ->
-              raise (Stuck e)
-        end
-
-      | e ->
-          raise (Stuck e)
-    end
+ fun env e ->
+  match e with
+  | Num x -> Num x
+  | Str s -> Str s
+  | Sym var -> (
+      match Symtab.find_opt var env with
+      | Some value -> value
+      | None -> raise (Stuck e))
+  | Lst [ Sym "let"; Lst [ Lst [ Sym var; exp ] ]; body ] ->
+      let env = env |> Symtab.add var (interp_expr env exp) in
+      interp_expr env body
+  | Lst [ Sym "if"; test_exp; then_exp; else_exp ] ->
+      if interp_expr env test_exp <> Bool false then interp_expr env then_exp
+      else interp_expr env else_exp
+  | Lst (Sym "do" :: exps) when List.length exps > 0 ->
+      exps |> List.rev_map (interp_expr env) |> List.hd
+  | Lst [ Sym f ] -> (
+      match interp_0ary_primitive f with Some v -> v | None -> raise (Stuck e))
+  | Lst [ Sym f; arg ] -> (
+      match interp_unary_primitive f (interp_expr env arg) with
+      | Some v -> v
+      | None -> raise (Stuck e))
+  | Lst [ Sym f; arg1; arg2 ] -> (
+      match
+        let v1 = interp_expr env arg1 in
+        let v2 = interp_expr env arg2 in
+        interp_binary_primitive f v1 v2
+      with
+      | Some v -> v
+      | None -> raise (Stuck e))
+  | Lst [ Sym f; arg1; arg2; arg3 ] -> (
+      match
+        let v1 = interp_expr env arg1 in
+        let v2 = interp_expr env arg2 in
+        let v3 = interp_expr env arg3 in
+        interp_trinary_primitive f v1 v2 v3
+      with
+      | Some v -> v
+      | None -> raise (Stuck e))
+  | e -> raise (Stuck e)
 
 (** [interp e] evaluates the s-expression [e] using [interp_expr], reading input
     from stdin and writing output to stdout. *)
-let interp : s_exp -> unit =
-  fun e ->
-    e
-      |> interp_expr (top_env ())
-      |> ignore
+let interp : s_exp -> unit = fun e -> e |> interp_expr (top_env ()) |> ignore
 
 (** [interp_io e input] evaluates the s-expression [e] using [interp_expr],
    reading input from the string [input] and returning the output as a
---
+++ lib/runtime.c
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define STDIN_FD 0
+#define STDOUT_FD 1
+
+/* [read_all(fd, buf, n)] reads exactly [n] bytes of data from the file
+ * descriptor [fd] into [buf], then adds a null byte at the end. Prints an
+ * error and exits if there is a problem with a read (for instance, if the file
+ * descriptor isn't open for reading).
+ */
+void read_all(int fd, char *buf, int n) {
+  int total_read = 0;
+  while (total_read < n) {
+    int read_this_time = read(fd, buf + total_read, n - total_read);
+    if (read_this_time < 0) {
+      printf("Read error\n");
+      exit(1);
+    } else if (read_this_time == 0) {
+      printf("Unexpected EOF\n");
+      exit(1);
+    }
+    total_read += read_this_time;
+  }
+  buf[n] = '\0';
+}
+
+/* [write_all(fd, buf)] writes the null_terminated string [buf] to the file
+ * descriptor [fd]. Prints an error and exits if there is a problem with a
+ * write (for instance, if the file descriptor isn't open for writing).
+ */
+void write_all(int fd, char *buf) {
+  int total = strlen(buf);
+  int total_written = 0;
+  while (total_written < total) {
+    int written_this_time = write(fd, buf + total_written, total - total_written);
+    if (written_this_time < 0) {
+      printf("Write error\n");
+      exit(1);
+    }
+    total_written += written_this_time;
+  }
+}
+
+/* [open_for_reading(filename)] opens the file named [filename] in read-only
+ * mode and returns a file descriptor, printing an error and exiting if there
+ * is a problem (e.g., if the file does not exist).
+ */
+int open_for_reading(char *filename) {
+  int fd = open(filename, O_RDONLY);
+  if (fd < 0) {
+    printf("Open error\n");
+    exit(1);
+  }
+  return fd;
+}
+
+/* [open_for_writing(filename)] opens the file named [filename] in write-only
+ * mode (creating it if it doesn't exist) and returns a file descriptor,
+ * printing an error and exiting if there is a problem (e.g., if the file is in
+ * a directory that doesn't exist).
+ */
+int open_for_writing(char *filename) {
+  int fd = open(filename, O_WRONLY | O_CREAT, 0644);
+  if (fd < 0) {
+    printf("Open error\n");
+    exit(1);
+  }
+  return fd;
+}
+
+extern void lisp_entry(void *heap);
+
+#define num_mask   0b11
+#define num_tag    0b00
+#define num_shift  2
+
+#define bool_mask  0b1111111
+#define bool_tag   0b0011111
+#define bool_shift 7
+
+#define heap_mask 0b111
+#define pair_tag 0b010
+
+#define string_mask 0b111
+#define string_tag 0b011
+
+#define inchannel_mask 0b111111111
+#define inchannel_tag 0b011111111
+#define outchannel_mask 0b111111111
+#define outchannel_tag 0b001111111
+#define inchannel_shift 9
+#define outchannel_shift 9
+
+// Channel operations
+int64_t open_in_channel(char* filename) {
+  int fd = open_for_reading(filename);
+  if (fd < 0) return -1;
+  return ((int64_t)fd << 9) | 0b011111111;
+}
+
+int64_t open_out_channel(char* filename) {
+  int fd = open_for_writing(filename);
+  if (fd < 0) return -1;
+  return ((int64_t)fd << 9) | 0b001111111;
+}
+
+int64_t close_in_channel(int64_t channel) {
+  int fd = channel >> 9;
+  close(fd);
+  return 1;
+}
+
+int64_t close_out_channel(int64_t channel) {
+  int fd = channel >> 9;
+  close(fd);
+  return 1;
+}
+
+int64_t input_channel(int64_t channel, int64_t n, void* heap) {
+  // Check if it's an input channel
+  if ((channel & inchannel_mask) != inchannel_tag) {
+      printf("Input error: not an input channel\n");
+      exit(1);
+  }
+  
+  // Check if n is a valid number
+  if ((n & num_mask) != num_tag) {
+      printf("Input error: invalid length\n");
+      exit(1);
+  }
+
+  int fd = (channel & ~inchannel_mask) >> 9;
+  int bytes_to_read = n >> num_shift;
+
+  // Ensure heap is 8-byte aligned
+  uintptr_t aligned_heap = (uintptr_t)heap;
+  if (aligned_heap % 8 != 0) {
+      aligned_heap += (8 - (aligned_heap % 8));
+  }
+
+  read_all(fd, (char*)aligned_heap, bytes_to_read);
+  
+  return ((int64_t)aligned_heap | string_tag);
+}
+
+int64_t output_channel(int64_t channel, int64_t str) {
+  // Check if it's an output channel
+  if ((channel & outchannel_mask) != outchannel_tag) {
+      printf("Output error: not an output channel\n");
+      exit(1);
+  }
+
+  // Check if str is a string
+  if ((str & string_mask) != string_tag) {
+      printf("Output error: not a string\n");
+      exit(1);
+  }
+
+  int fd = (channel & ~outchannel_mask) >> 9;
+  char* string_ptr = (char*)(str - string_tag);
+  
+  write_all(fd, string_ptr);
+  
+  return (1LL << bool_shift) | bool_tag;
+}
+
+void print_value(uint64_t value) {
+  if ((value & num_mask) == num_tag) {
+    int64_t ivalue = (int64_t)value;
+    printf("%" PRIi64, ivalue >> num_shift);
+  } else if ((value & bool_mask) == bool_tag) {
+    if (value >> bool_shift) {
+      printf("true");
+    }
+    else {
+      printf("false");
+    }
+  } else if ((value & heap_mask) == pair_tag) {
+    uint64_t v1 = *(uint64_t *)(value - pair_tag);
+    uint64_t v2 = *(uint64_t *)(value - pair_tag + 8);
+    printf("(pair ");
+    print_value(v1);
+    printf(" ");
+    print_value(v2);
+    printf(")");
+  } else if ((value & string_mask) == string_tag) {
+    char *str = (char *)(value - string_tag);
+    printf("\"");  // Print opening quote
+    while (*str != '\0') {
+      switch (*str) {
+        case '"':
+            printf("\\\"");
+            break;
+        case '\n':
+            printf("\\n");
+            break;
+        default:
+            putchar(*str);
+    }
+        str++;
+    }
+    printf("\"");  // Print closing quote
+}
+else if ((value & inchannel_mask) == inchannel_tag) {
+  printf("<in-channel>");
+} else if ((value & outchannel_mask) == outchannel_tag) {
+  printf("<out-channel>");
+}}
+
+void lisp_error(char *exp) {
+  printf("Stuck[%s]", exp);
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  void *heap = malloc(4096);
+  lisp_entry(heap);
+  return 0;
+}
+
+uint64_t read_num() {
+  int r;
+  scanf("%d", &r);
+  return (uint64_t)(r) << num_shift;
+}
+
+void print_newline() {
+  printf("\n");
+}
